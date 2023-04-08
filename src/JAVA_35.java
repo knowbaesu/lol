8fc08200d019cc7b5deabb3a1c66e51ec96abe8d
pjs <vkrwltn12@gmail.com>
2023-04-08 23:55:35 +0900

    u

---
+++ src/JAVA_35.java
@@ -0,0 +1,14 @@
+package JAVA_0308;
+
+public class JAVA_35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.min(3, 13));
+		System.out.println(Math.max(123, 54));
+		System.out.println(Math.abs(-23.1));
+		System.out.println(Math.sqrt(3));
+		System.out.println(Math.random());
+	}
+
+}
