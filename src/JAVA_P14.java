27bc57dfa042ec04128009aa974e51cb0d865c91
pjs <vkrwltn12@gmail.com>
2023-04-10 22:59:47 +0900

    s

---
+++ src/JAVA_P14.java
@@ -0,0 +1,45 @@
+package JAVA_0308;
+
+public class JAVA_P14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "This is a apple ";
+		
+		
+	
+		int d = 0;
+		
+		char[] c = text.toCharArray();
+		System.out.println((byte)text.charAt(4));
+		for (int i=0; i<text.length();i++) {
+			
+			if( (byte)(text.charAt(i)) == 97 ) {
+				if((byte)(text.charAt(i+1)) == 32 && (byte)(text.charAt(i+2)) == 97 ) {
+					d++;
+					
+					
+					
+				} else if ((byte)(text.charAt(i)+1) == 32 && (byte)(text.charAt(i)+2) == 101) {
+					
+					System.out.println();
+				} else if ((byte)(text.charAt(i)+1) == 32 && (byte)(text.charAt(i)+2) == 105) {
+					
+					
+				} else if ((byte)(text.charAt(i)+1) == 32 && (byte)(text.charAt(i)+2) == 111) {
+					
+					
+				} else if ((byte)(text.charAt(i)+1) == 32 && (byte)(text.charAt(i)+2) == 117) {
+					
+				
+				}
+			} 
+			
+			System.out.print(d);
+			
+			
+		} 
+		
+	}
+
+}
