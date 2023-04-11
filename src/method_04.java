1324f1337e5ace5c8d0f1681fd40f597c8cad092
pjs <vkrwltn12@gmail.com>
2023-04-11 23:51:32 +0900

    method

---
+++ src/method_04.java
@@ -0,0 +1,16 @@
+package java09;
+
+public class method_04 {
+
+	public static void main(String[] args) {
+		System.out.println(addOperation(1,2));
+
+	}
+
+	
+	
+	public static int addOperation(int a, int b) {
+		int sum = a+b;
+		return sum;
+	}
+}
