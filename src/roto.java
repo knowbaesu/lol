ab9b23a3b2251a87ad828bb844c2b96e180c391f
pjs <vkrwltn12@gmail.com>
2023-04-12 23:37:26 +0900

    roto

---
+++ src/roto.java
@@ -0,0 +1,15 @@
+package JAVA_0308;
+
+public class roto {
+
+	public static void main(String[] args) {
+		
+	for (int i=0; i<6; i++) {
+		int num = (int)(Math.random()*45);
+		System.out.println(num);
+	}
+		
+
+	}
+
+}
