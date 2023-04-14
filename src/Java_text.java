c2d07bb3b800bc697ffcf7cd12db93db1616530b
pjs <vkrwltn12@gmail.com>
2023-04-14 23:51:46 +0900

    text

---
+++ src/Java_text.java
@@ -0,0 +1,27 @@
+package JAVA_0308;
+
+import java.util.Scanner;
+
+public class Java_text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String  text =  scanner.nextInt();
+		for (int i = a~z) {
+			char a ..
+			int cnt = 0;
+			 for (int i = 0; i < text.length(); i++ ) {
+				 
+			 }
+		}
+		
+		
+		
+		for (int i =0; i < text.lenghth(); i++ ) {
+			if(text.charAt(i))
+		}
+		
+	}
+
+}
