c41233d9636923d213da4d349fa1fbd5e7e93f8c
pjs <vkrwltn12@gmail.com>
2023-04-16 23:54:20 +0900

    a5

---
+++ src/java_a5.java
@@ -0,0 +1,22 @@
+package java07;
+
+import java.util.Scanner;
+
+public class java_a5 {
+
+	  public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int n = scan.nextInt();
+
+	        for(int i=0;i<n;i++){
+	            for(int j=0;j<i;j++){
+	                System.out.print(" ");
+	            }
+
+	            for(int j=0;j<(2*n-1)-(2*i);j++){
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	  }
+}
